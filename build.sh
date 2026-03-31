833a08d99f322c0ef967f306b0dbf7b132d577a7
Amirthakatesan M <119378816+Amirthakatesan57@users.noreply.github.com>
2026-03-31 12:12:50 +0530

    Modify build script for admin and demo data
    Comment out the admin creation script and enable demo data seeding.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 
 # 2. Run database setup scripts
 python scripts/init_db.py
-python scripts/create_admin.py
+# python scripts/create_admin.py
 
 # Optional: Comment this out if you don't want fake test data in your live app
-# python scripts/seed_demo_data.py
+python scripts/seed_demo_data.py
